fee69569757142b3e5c2cc3618e3dba712818276
🜲 Wizard / <165882146+Wizard18774318@users.noreply.github.com>
2026-02-28 00:40:50 +0100

    works collision

--- Assets/Scripts/ObjectDestroyer.cs
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -4,7 +4,7 @@ public class ObjectDestroyerColllisionDetection : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(other.gameObject);
+        Destroy(gameObject);
     }
 }
 
