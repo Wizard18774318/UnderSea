5ccee9c85cc29684c61b5c72aa2571c0d7016550
🜲 Wizard / <165882146+Wizard18774318@users.noreply.github.com>
2026-02-28 19:36:44 +0100

    quality changes

--- Assets/Scripts/DestreyItselfOnTrigger.cs
+++ Assets/Scripts/DestreyItselfOnTrigger.cs
@@ -4,7 +4,9 @@ public class ObjectGetsDestroyed : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Destroyer") || (other.CompareTag("Projectile") && gameObject.CompareTag("Enemy_Projectile")))
+        if (other.CompareTag("Destroyer") 
+        || (other.CompareTag("Projectile") && gameObject.CompareTag("Enemy_Projectile")) 
+        || (other.CompareTag("Enemy_Projectile") && gameObject.CompareTag("Projectile")))
         {
             Destroy(gameObject);
         }
